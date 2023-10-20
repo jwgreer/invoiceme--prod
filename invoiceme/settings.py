1d83e2a8375bb8ec264f9197e1d3a1a6ac117772
Jordan Greer <jwgreer2@gmail.com>
2023-10-20 14:33:00 -0600

    added more views

--- invoiceme/settings.py
+++ invoiceme/settings.py
@@ -33,11 +33,11 @@ SECRET_KEY = 'django-insecure-1&98=t98g6eq-k3br0qy4gi^4d89y5_h9%v622-)k(+3j4jzww
 DEBUG = False
 
 if DEBUG:
-    SITE_URL = 'http://www.invoiceme.site'
-else:
     SITE_URL = 'http://localhost:8000'
+else:
+    SITE_URL = 'http://www.invoiceme.site'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['invoiceme.site', 'jordan.invoiceme.site', 'localhost:8000']
 
 
 # Application definition
