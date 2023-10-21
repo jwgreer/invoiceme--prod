9c17d856c4f0092421b77e6ed354a6dc19199f6f
Jordan Greer <jwgreer2@gmail.com>
2023-10-20 19:40:20 -0600

    added more views

--- static/js/addItems.js
+++ static/js/addItems.js
@@ -1,44 +1,39 @@
-console.log(invoice_pk);
-console.log(site_url);
-console.log(apiUrl);
 
-var api = apiUrl
-console.log(api)
-
+var api = apiUrl;
 
 $(document).ready(function () {
     $(document).on('click', '.add-item', function () {
-        var form = $(this).closest('form');  // You can update this with the appropriate invoice value
+        var form = $(this).closest('form');
         var product = form.find('input[name="product"]').val();
         var quantity = form.find('input[name="quantity"]').val();
         var csrfToken = form.find('input[name="csrfmiddlewaretoken"]').val();
-        var apiUrl = apiUrl;  // Access the URL from the data-url attribute
+        var apiUrl = apiUrl;
 
         $.ajax({
             type: 'POST',
-            url: apiUrl,  // Use the dynamically generated URL
+            url: apiUrl,
             data: {
                 invoice: invoice_pk,
                 product: product,
                 quantity: quantity,
                 csrfmiddlewaretoken: csrfToken,
             },
-            success: function () {
-                populateTable(apiUrl)
+            success: function (response) {
+                populateTable(apiUrl);
                 // Add code here to update the page as needed, e.g., displaying the newly added item
             }
         });
     });
 });
 
-
-function populateTable(api) { // Replace with the correct URL
-    var csrfToken = crsfToken; // Replace with the correct CSRF token
+function populateTable(apiUrl) {
+    var csrfToken = crsfToken;
     $.ajax({
         type: 'GET',
         url: apiUrl,
         success: function (data) {
-            console.log(data); // Log the response directly
+            // Remove all console.log statements
+            // Add code here to update the page as needed
         }
     });
 }
